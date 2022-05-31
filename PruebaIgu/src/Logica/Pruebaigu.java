52cbebd2f3707291450438cfdca12c961ff31e2e
nazarenocabral <cabral.en@gmail.com>
2022-05-30 23:03:55 -0300

    estoy seguro

---
+++ PruebaIgu/src/Logica/Pruebaigu.java
@@ -0,0 +1,20 @@
+
+package Logica;
+
+import Igu.Principal;
+
+
+public class Pruebaigu {
+
+
+    public static void main(String[] args) {
+          
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
