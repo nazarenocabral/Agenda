172870dc7cc24a8e5688c1cd322fc84cd7bb997f
nazarenocabral <cabral.en@gmail.com>
2022-06-04 18:13:54 -0300

    Agenda electronica sin base de datos

---
+++ agendaElectronica/src/Logica/AgendaElectronica.java
@@ -0,0 +1,18 @@
+
+package Logica;
+
+import IGU.Salida;
+
+
+public class AgendaElectronica {
+
+    
+    public static void main(String[] args) {
+        Salida pantalla = new Salida();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
